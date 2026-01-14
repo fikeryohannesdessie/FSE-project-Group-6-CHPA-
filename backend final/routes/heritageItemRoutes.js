3fb80bd5c8ece5dc34401946686cb1c75a8d9356
fikeryohannesdessie <fikeryohannes91@gmail.com>
2026-01-14 20:30:54 +0300

    heritageItemRoutes.js

--- backend final/routes/heritageItemRoutes.js
+++ backend final/routes/heritageItemRoutes.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
-
+const { uploadFile } = require("../middleware/upload");
 const heritageItemController = require("../controllers/heritageItemController");
 const { requireRole } = require("../middleware/authorize");
 
 router.post(
   "/items",
-  // requireRole("contributor"),
+  uploadFile.single("media"),
   heritageItemController.createItem
+);
+
+router.get("/items", heritageItemController.getApprovedItems);
+router.get("/items/search", heritageItemController.searchItems);
+router.get("/items/filter", heritageItemController.filterByLanguage);
+
+router.get(
+  "/items/pending",
+  requireRole("admin"),
+  heritageItemController.getPendingItems
 );
 
 router.put(
@@ -17,11 +27,3 @@ router.put(
 );
 
 module.exports = router;
-router.get("/items", heritageItemController.getApprovedItems);
-router.get("/items/search", heritageItemController.searchItems);
-router.get("/items/filter", heritageItemController.filterByLanguage);
-router.get(
-  "/items/pending",
-  requireRole("admin"),
-  heritageItemController.getPendingItems
-);
